14780d13943427439825138bcb56bd085294c111
BasmaEsmail <52723710+BasmaEsmail@users.noreply.github.com>
2022-05-12 17:51:46 +0200

    Update products.js

--- js/products.js
+++ js/products.js
@@ -36,36 +36,67 @@ var toolsJson= [
         price:"80 EGP"
     }
 ]
+function search_Product() {
+    let input = document.getElementById('inpu').value;
+    console.log(input)
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('card');
+      
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display="list-item";
+            x[i].style.listStyleType="none";                 
+        }
+    }
+}
+
+
+    
 var row= document.getElementById("row");
 var img,card, icon,icon2;
-
+function getCart()
+{
+    var s=localStorage.getItem("myOrders");
+            console.log(s)
+}
 for(var i =0;i<toolsJson.length;i++)
 {
+        ///// CREATE CARDS //////
     card=document.createElement("div");
     card.setAttribute("class","card");
     card.setAttribute("onmouseover","openControls(this)")
     card.setAttribute("onmouseout","closeControls(this)")
 
+        ///// CREATE ICONS/////
     icon= document.createElement("i");
     icon2=document.createElement("i")
     icon.setAttribute("class","fa-solid fa-heart");
     icon2.setAttribute("class","fa-solid fa-cart-plus ");
+    icon2.setAttribute("id",toolsJson[i].id);
+    icon2.setAttribute("flag","true");
+    icon2.setAttribute("onclick","add(this)");
+
+        //// CREATE SPAN OF ICONS/////
     controls=document.createElement("span");
     controls.setAttribute("style","display:none");
     controls.setAttribute("class","controls")
     controls.appendChild(icon)
     controls.appendChild(icon2)
 
-
+        ///// CREATE CARDS IMAGES/////
     img= document.createElement("img");
     img.setAttribute("src",toolsJson[i].image)
     img.setAttribute("id","toolImg")
     
-
+    //// CREATE CARD TEXT
     var head=document.createElement("h3");
     head.innerHTML= toolsJson[i].toolName;
     head.setAttribute("class","head-prod");
 
+    ///// APPEND ALL/////
     var par=document.createElement("p");
     par.innerHTML= toolsJson[i].price;
     card.appendChild(img);
@@ -84,3 +115,31 @@ function closeControls(self)
 {
     self.lastChild.setAttribute("style","display:none")
 }
+
+/////////// ADD TO CART ////////////
+var orders=[]
+function add(self) {
+    var flag= self.getAttribute("flag")
+    if(flag=="true")
+        {
+            self.style.color="#ffff00"
+            for(var i=0;i<toolsJson.length;i++)
+            {
+            if(self.id==toolsJson[i].id)
+            { 
+                orders.push(toolsJson[i])
+            }
+            } 
+            self.setAttribute("flag","false");
+        }
+        else 
+        {
+            self.style.color="white";
+            orders.pop();
+            self.setAttribute("flag","true");
+        }
+        localStorage.setItem("myOrders",JSON.stringify(orders));
+        console.log(JSON.parse(localStorage.getItem("myOrders")))
+
+  }
+
