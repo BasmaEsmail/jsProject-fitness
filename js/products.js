96d6d3ddebf7316bc952cca17790a3c1e78014a3
BasmaEsmail <52723710+BasmaEsmail@users.noreply.github.com>
2022-05-13 23:48:41 +0200

    Update products.js

--- js/products.js
+++ js/products.js
@@ -36,6 +36,7 @@ var toolsJson= [
         price:"80 EGP"
     }
 ]
+////////////// SEARCH
 function search_Product() {
     let input = document.getElementById('inpu').value;
     console.log(input)
@@ -53,7 +54,78 @@ function search_Product() {
     }
 }
 
+//--filterrrrrrrrrrrrrrrrrring
+function Filter1(){
+    let Dumbell = document.getElementById('Dumbel').value;
+    let x = document.getElementsByClassName('card');
+    console.log(x);
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].innerHTML.includes(document.getElementById("Dumbel"))) {
+            x[1].style.display="none";
+            x[2].style.display="none";
+            x[3].style.display="none";
+            x[4].style.display="none";
+            x[5].style.display="none";
+        }
+        else {
+            x[i].style.display="list-item";                 
+        }
+    }
+}
 
+function Filter2(){
+    let Dumbell = document.getElementById('Dumbel').value;
+    let x = document.getElementsByClassName('card');
+    console.log(x);
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].innerHTML.includes(document.getElementById("Dumbel"))) {
+            x[0].style.display="none";
+            x[2].style.display="none";
+            x[3].style.display="none";
+            x[4].style.display="none";
+            x[5].style.display="none";
+        }
+        else {
+            x[i].style.display="list-item";                 
+        }
+    }
+}
+
+function Filter3(){
+    let Dumbell = document.getElementById('Dumbel').value;
+    let x = document.getElementsByClassName('card');
+    console.log(x);
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].innerHTML.includes(document.getElementById("Dumbel"))) {
+            x[0].style.display="none";
+            x[1].style.display="none";
+            x[2].style.display="none";
+            x[4].style.display="none";
+            x[5].style.display="none";
+        }
+        else {
+            x[i].style.display="list-item";                 
+        }
+    }
+}
+
+function Filter4(){
+    let Dumbell = document.getElementById('Dumbel').value;
+    let x = document.getElementsByClassName('card');
+    console.log(x);
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].innerHTML.includes(document.getElementById("Dumbel"))) {
+            x[0].style.display="none";
+            x[1].style.display="none";
+            x[3].style.display="none";
+            x[4].style.display="none";
+            x[5].style.display="none";
+        }
+        else {
+            x[i].style.display="list-item";                 
+        }
+    }
+}
     
 var row= document.getElementById("row");
 var img,card, icon,icon2;
@@ -74,15 +146,18 @@ for(var i =0;i<toolsJson.length;i++)
     icon= document.createElement("i");
     icon2=document.createElement("i")
     icon.setAttribute("class","fa-solid fa-heart");
+    icon.setAttribute("flag2","true");
+    icon.setAttribute("onclick","addFavourite(event)");
+    
     icon2.setAttribute("class","fa-solid fa-cart-plus ");
-    icon2.setAttribute("id",toolsJson[i].id);
     icon2.setAttribute("flag","true");
-    icon2.setAttribute("onclick","add(this)");
+    icon2.setAttribute("onclick","add(event)");
 
         //// CREATE SPAN OF ICONS/////
     controls=document.createElement("span");
     controls.setAttribute("style","display:none");
     controls.setAttribute("class","controls")
+    controls.setAttribute("id",toolsJson[i].id)
     controls.appendChild(icon)
     controls.appendChild(icon2)
 
@@ -118,28 +193,56 @@ function closeControls(self)
 
 /////////// ADD TO CART ////////////
 var orders=[]
-function add(self) {
-    var flag= self.getAttribute("flag")
+function add(event) {
+    var flag= event.target.getAttribute("flag")
     if(flag=="true")
         {
-            self.style.color="#ffff00"
+            event.target.style.color="#ffff00"
             for(var i=0;i<toolsJson.length;i++)
             {
-            if(self.id==toolsJson[i].id)
+            if(event.target.parentNode.id==toolsJson[i].id)
             { 
                 orders.push(toolsJson[i])
             }
             } 
-            self.setAttribute("flag","false");
+            event.target.setAttribute("flag","false");
         }
         else 
         {
-            self.style.color="white";
+            event.target.style.color="white";
             orders.pop();
-            self.setAttribute("flag","true");
+            event.target.setAttribute("flag","true");
         }
         localStorage.setItem("myOrders",JSON.stringify(orders));
-        console.log(JSON.parse(localStorage.getItem("myOrders")))
-
   }
+
+  //favourite
+
+
+
+  var favourites=[]
+  function addFavourite(event) {
+      var flag2= event.target.getAttribute("flag2")
+      if(flag2=="true")
+          {
+              event.target.style.color="red"
+              for(var i=0;i<toolsJson.length;i++)
+              {
+              if(event.target.parentNode.id==toolsJson[i].id)
+              { 
+                  favourites.push(toolsJson[i])
+              }
+              } 
+              event.target.setAttribute("flag2","false");
+          }
+          else 
+          {
+              event.target.style.color="white";
+              favourites.pop();
+              event.target.setAttribute("flag2","true");
+          }
+          localStorage.setItem("favourites",JSON.stringify(favourites));
+        console.log(JSON.parse(localStorage.getItem("favourites")))
+
+    }
 
